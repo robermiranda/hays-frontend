dd951af1be5b33b9fe17d0aeeaf4ec83594d40bc
Rober Miranda <rob.mirandam@gmail.com>
2023-05-12 08:49:14 -0600

    formating info into a table

--- src/components/TiendaList.tsx
+++ src/components/TiendaList.tsx
@@ -1,9 +1,59 @@
 import { Box } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import { tiendaT } from '../util/types';
 import {  useRecoilValue } from 'recoil';
 import { tiendaListAtom } from '../util/state';
 
 
+export default function TiendaList () {
+
+    const locations = useRecoilValue(tiendaListAtom);
+
+    return (
+        <TableContainer component={Paper}>
+            <Table aria-label="detail table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell align="right">LAT</TableCell>
+                        <TableCell align="right">LNG</TableCell>
+                        <TableCell align="right">TITULO</TableCell>
+                        <TableCell align="right">TIPO</TableCell>
+                        <TableCell align="right">GERENTE</TableCell>
+                        <TableCell align="right">DIRECCIÓN</TableCell>
+                        <TableCell align="right">TELÉFONO</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {locations.map((location: tiendaT) =>
+                        <TableRow key={location.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+
+                            <TableCell component="th" scope="row">
+                                {location.id}
+                            </TableCell>
+                            <TableCell align="right">{location.lat}</TableCell>
+                            <TableCell align="right">{location.lng}</TableCell>
+                            <TableCell align="right">{location.titulo}</TableCell>
+                            <TableCell align="right">{location.tipo}</TableCell>
+                            <TableCell align="right">{location.gerente}</TableCell>
+                            <TableCell align="right">{location.direccion}</TableCell>
+                            <TableCell align="right">{location.telefono}</TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+/*
 export default function TiendaList () {
 
     const locations = useRecoilValue(tiendaListAtom);
@@ -26,3 +76,34 @@ export default function TiendaList () {
         }
     </>
 }
+
+export default function InfoTable ({info}: any) {
+
+    const keys = Object.keys(info);
+
+    return (
+        <TableContainer component={Paper}>
+            <Table aria-label="detail table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Property</TableCell>
+                        <TableCell align="right">Value</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {keys.map((key: string) => (
+                        <TableRow key={key}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell component="th" scope="row">
+                                {key}
+                            </TableCell>
+                            <TableCell align="right">
+                                {info[key]}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}*/
